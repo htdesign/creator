b7e6a8d5a0940495916cf92a6ce90c3ba98ad71b
Hal Trazo <h.webdesigns@gmail.com>
2021-12-20 16:49:42 +1300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./dist/**/*.html"],
+  theme: {
+    fontFamily: {
+      'sans': ['"Segoe UI"', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'ms-blue': '#0072c9',
+        'ms-blue-hover': '#0060a9',
+        'ms-blue-active': '#097dd5',
+        'ms-blue-focus': '#0072c9',
+      },
+    },
+  },
+  plugins: [],
+}
